e730800ecc03cb48763b64b59170c4b47d39d3b7
facupetitfour <facundopetitfour1@gmail.com>
2024-06-25 18:43:31 -0300

    version 0.0.1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,30 +2,73 @@ import { useEffect, useState } from "react";
 import NavBar from "../component/NavBar";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const serverhost = "http://localhost:3000/"
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+  Container,
+} from "@mui/material";
 
+const serverhost = "http://localhost:3000/";
 
 function Home() {
-  const navigate = useNavigate()
-  const [data,setData] = useState({})
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
 
-  useEffect(()=>{
-    const getData = async()=>{
+  useEffect(() => {
+    const getData = async () => {
       try {
-        const response = await axios.get(serverhost+"users",{withCredentials:true})
-        setData(response.data)
+        const response = await axios.get(serverhost + "products", {
+          withCredentials: true,
+        });
+        console.log(response.data)
+        setData(response.data);
       } catch (error) {
-        console.error("Error al obtener data de usuarios",error);
-        navigate("/login")
+        console.error("Error al obtener data de usuarios", error);
+        navigate("/login");
       }
-    }
-    getData()
-  },[navigate])
+    };
+    getData();
+  }, [navigate]);
 
   return (
     <>
       <NavBar />
-      <div>Home</div>
+      {data ? (
+        <Container sx={{ mt: 4 }}>
+          <Grid container spacing={4}>
+            {data.map((product) => (
+              <Grid item key={product._id} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{ height: 140 }}
+                    image={product.img}
+                    alt={product.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {product.name}
+                    </Typography>
+                    <Typography>{product.description}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      ${product.price}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      ) : null}
     </>
   );
 }
